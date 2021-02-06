b8b36f2fe76b79110001083279035c8489b20c56
Jorge Aguilera <jorge.aguilera@puravida-software.com>
2021-02-06 19:50:40 +0100

    add quote to ciberado

--- scripts/ciberado.groovy
+++ scripts/ciberado.groovy
@@ -1,12 +1,26 @@
 @Grab(group='org.twitter4j', module='twitter4j-core', version='4.0.6')
+@Grab('org.ccil.cowan.tagsoup:tagsoup:1.2.1')
+import org.ccil.cowan.tagsoup.Parser
 
 import twitter4j.TwitterFactory
 import twitter4j.StatusUpdate
 import java.util.Locale
 import java.text.SimpleDateFormat
 
+html = "https://es.wikiquote.org/wiki/Portada".toURL().text 
+def slurper = new XmlSlurper(new Parser() )
+def document = slurper.parseText(html)
+quote = document.'**'.find { it['@id'] == 'toc' }.table.tbody.'*'.findAll{ it.name() == 'tr'}.first().text()
+author = document.'**'.find { it['@id'] == 'toc' }.table.tbody.'*'.findAll{ it.name() == 'tr'}.last().td.div.a.text()
+link  = "https://es.wikiquote.org/"+document.'**'.find { it['@id'] == 'toc' }.table.tbody.'*'.findAll{ it.name() == 'tr'}.last().td.div.a['@href']
 
-status = new StatusUpdate("Aquí tienes tu magdalena diaria @ciberado, que aproveche")
+status = new StatusUpdate("""Aquí tienes tu magdalena diaria @ciberado, que aproveche
+
+$quote
+($author $link)
+
+#wikiquote""")
+
 files = new File("images").listFiles()
 rnd = new Random()
 file = files[rnd.nextInt(files.length)]
