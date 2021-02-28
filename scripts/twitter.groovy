b42e666e5b25f6e9b81b31cafb01a4be7f7124b1
Jorge Aguilera <jorge.aguilera@puravida-software.com>
2021-02-28 23:35:35 +0100

    mojon

--- scripts/twitter.groovy
+++ scripts/twitter.groovy
@@ -9,26 +9,21 @@ import java.text.SimpleDateFormat
 
 today = Calendar.instance
 
-videos = [
-    "https://youtu.be/4EIGbQaEIN0",
-    "https://youtu.be/1AnG04qnLqI",
-    "https://youtu.be/U_44YUDFvuI",
-    "https://youtu.be/Wm4YqE2XQwM",
-    "https://youtu.be/MVkDDSz_gE4",
-    "https://youtu.be/UDPUPJUumXE",    
-]
-
 losdemarras = "https://youtu.be/Iu1bKskocNQ"
 
 txt = ''
 video = ''
+bytes = null
 if( today.get(Calendar.DAY_OF_WEEK) == Calendar.FRIDAY ){
     txt = 'Hoy sí que es viernes, salimos a las 3 y NO hay paso a producción'
     video = losdemarras
 }else{
-    txt = "Vale, hoy a lo mejor no es viernes, ni salimos a las 3 pero NO hay paso a producción"
+    files = new File("images/mojon").listFiles()
     rnd = new Random()
-    video = videos[ rnd.nextInt(videos.size()) ]
+    file = files[rnd.nextInt(files.length)]
+    bytes = file.bytes
+
+    txt = "Tremendo mojon tienes por delante"    
 }
 
 message = ""
@@ -48,11 +43,11 @@ message += txt
 message += "\n\n"
 message += video
 
+
 status = new StatusUpdate(message)
-if( args.length > 1 ){
-    def bytes = args[1].toURL().bytes
+if( bytes )
     status.media "image", new ByteArrayInputStream(bytes)
-}
+
 println status.status
 TwitterFactory.singleton.updateStatus(status)
 
